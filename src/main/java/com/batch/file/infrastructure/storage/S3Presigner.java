974c5cc985e1bcb5736728c8064cfef83a9bc82a
gaurav <gauravmahajan001@gmail.com>
2026-06-16 21:15:15 +0530

    updated file name

--- src/main/java/com/batch/file/infrastructure/storage/S3Presigner.java
+++ src/main/java/com/batch/file/infrastructure/storage/S3Presigner.java
@@ -5,21 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.regions.Region;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class S3PresignerConfig {
+public class S3Presigner {
 
     @Value("${aws.region:AP_SOUTH_1}")
     private String awsRegion;
 
     @Bean
-    public S3Presigner s3Presigner() {
+    public software.amazon.awssdk.services.s3.presigner.S3Presigner s3Presigner() {
         log.info("Initializing S3Presigner bean with region: {}", awsRegion);
-        return S3Presigner.builder()
+        return software.amazon.awssdk.services.s3.presigner.S3Presigner.builder()
                 .region(Region.of(awsRegion))
                 .build();
     }
