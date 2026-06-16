e3bc8e8120214d02470f035b5df0dfd6040d2131
gaurav <gauravmahajan001@gmail.com>
2026-06-16 22:55:03 +0530

    updated  import statement for @Nullable

--- src/main/java/com/batch/file/adapters/in/batch/BatchProcessor.java
+++ src/main/java/com/batch/file/adapters/in/batch/BatchProcessor.java
@@ -2,8 +2,10 @@ package com.batch.file.adapters.in.batch;
 
 import com.batch.file.entity.batch.Customer;
 import lombok.extern.slf4j.Slf4j;
-import org.jspecify.annotations.Nullable;
+
 import org.springframework.batch.item.ItemProcessor;
+
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
